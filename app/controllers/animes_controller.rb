7e2c52d7e62884de4deeccbaabac2480a06befd2
Joshua Yee <joshyee16@gmail.com>
2023-12-06 17:19:31 -0800

    dynamic show update

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -3,4 +3,9 @@ class AnimesController < ApplicationController
     @animes = Anime.all
     render template: "animes/index"
   end
+
+  def show 
+    @anime = Anime.find_by(id: params[:id])
+    render template: "animes/show"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/animes" => "animes#index"
+  get "/animes/:id" => "animes#show"
 end

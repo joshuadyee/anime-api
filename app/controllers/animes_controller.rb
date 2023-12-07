1ca08e79fa3bc42804562a51f9625bee119504c2
Joshua Yee <joshyee16@gmail.com>
2023-12-06 17:27:14 -0800

    make dynamic create route

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -8,4 +8,16 @@ class AnimesController < ApplicationController
     @anime = Anime.find_by(id: params[:id])
     render template: "animes/show"
   end
+
+  def create
+    @anime = Anime.create(
+      id: params[:id],
+      title: params[:title],
+      creator: params[:creator],
+      main_character: params[:main_character],
+      episode_count: params[:episode_count],
+      years_run: params[:years_run]
+    )
+    render template: "animes/show"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/animes" => "animes#index"
   get "/animes/:id" => "animes#show"
+  post "/animes" => "animes#create"
 end
